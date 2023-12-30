7293c9fcc9e80b862f969d54d19b17128aec5a67
Achmad zailani <growtopiagame875236@gmail.com>
2023-12-31 03:18:27 +0800

    azda

--- weather/fxmanifest.lua
+++ weather/fxmanifest.lua
@@ -2,10 +2,7 @@ fx_version "cerulean"
 games {"gta5", "rdr3"}
 rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
 
-name "weathersync"
-author "kibukj"
-description "Time and weather synchronization for FiveM and RedM"
-url "https://github.com/kibook/weathersync"
+
 
 ui_page "ui/index.html"
 
